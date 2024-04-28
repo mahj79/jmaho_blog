c97258d7c3d125d0c03c5525ff4067dc48ef10a6
unknown <mahonej79@gmail.com>
2024-04-28 18:43:00 -0500

    new changes to page

--- jacks_blog/app/page.tsx
+++ jacks_blog/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
         <p className="mt-12 mb-12 text-3xl text-center dark:text-white">
-        Hello and Welcome to my blog! Thanks for checking it out! 
+        Hello and Welcome to my blog!
         <span className="whitespace-nowrap">
           I'm <span className="font-bold">Jack</span>.
         </span>
