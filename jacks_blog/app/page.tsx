3b04ab3c879dad75299b17f2787df847fd854085
mahj79 <mahonej79@gmail.com>
2024-05-18 10:43:08 -0500

    updated page with some welcome text and layout with text changes for dark mode

--- jacks_blog/app/page.tsx
+++ jacks_blog/app/page.tsx
@@ -1,7 +1,13 @@
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="px-6 mx-auto">
+      <p className="mt-12 mb-12 text-3x; text-center dark:text-white">
+        Hell and Welcome!
+        <span className="whitespace-nowrap">
+          I'm <span className="font-bold">Jack!</span>
+        </span>
+      </p>
       
     </main>
   );
