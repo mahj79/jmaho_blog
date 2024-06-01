e2569f802cf4ee121e1a92d787a8e4382d69c881
mahj79 <mahonej79@gmail.com>
2024-06-01 12:16:02 -0500

    added return for link to function post in page

--- jacks_blog/app/posts/[postId]/page.tsx
+++ jacks_blog/app/posts/[postId]/page.tsx
@@ -1,4 +1,6 @@
-import { getSortedPostsData } from "@/lib/posts";
+import getFormattedDate from "@/lib/getFormattedDate";
+import { getPostData, getSortedPostsData } from "@/lib/posts";
+import Link from "next/link";
 import { notFound } from "next/navigation";
 
 export default function generateMetadata({params}: {params: { postId: string}}) {
@@ -28,7 +30,20 @@ export default async function Post({params}: {params: { postId: string}}) {
         return notFound()
     }
     
+    const {title, date, contentHtml} = await getPostData(postId);
+
+    const pubDate = getFormattedDate(date);
+
     return (
-        <div>page</div>
+        <main className="px-6 prose prose-xl prose-slate dark:prose-invert mx-auto">
+            <h1 className="text-3xl mt-4 mb-0">{title}</h1>
+            <p className="mt-0">{pubDate}</p>
+            <article>
+                <section dangerouslySetInnerHTML={{__html: contentHtml}}/>
+                <p>
+                    <Link href="/">Back to Home</Link>
+                </p>
+            </article>
+        </main>
     )
 }
