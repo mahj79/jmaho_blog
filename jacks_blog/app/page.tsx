a6437cced81c437c2fe5bf1299045c3ea4680b8d
mahj79 <mahonej79@gmail.com>
2024-06-05 18:32:32 -0500

    more styling changes to font size

--- jacks_blog/app/page.tsx
+++ jacks_blog/app/page.tsx
@@ -2,8 +2,8 @@ import Posts from "./components/Posts";
 
 export default function Home() {
   return (
-    <main className="px-6 mx-auto">
-      <p className="mt-12 mb-12 text-3x; text-center dark:text-white">
+    <main className="px-12 mx-auto">
+      <p className="mt-12 mb-12 text-lg text-center dark:text-white">
         Hello and Welcome!&nbsp;
         <span className="whitespace-nowrap">
           I&apos;m <span className="font-bold">Jack!</span>
