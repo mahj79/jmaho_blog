33fd7c3cf3094666e11f434940195b6bce743c71
mahj79 <mahonej79@gmail.com>
2024-06-05 18:21:19 -0500

    added my profile pic and change border radius to rounded-3xl

--- jacks_blog/app/components/MyProfilePic.tsx
+++ jacks_blog/app/components/MyProfilePic.tsx
@@ -5,8 +5,8 @@ export default function MyProfilePic() {
         <section className="w-full mx-auto">
             <Image
                 className="border-4 border-black dark:border-slate-500 drop-shadow-xl 
-                shadow-black rounded-full mx-auto mt-8"
-                src={"/images/Cleo Grumpy.ico.jpg"}
+                shadow-black rounded-3xl mx-auto mt-8"
+                src={"/images/vail_headshot.jpg"}
                 width={200}
                 height={200}
                 alt="Profile Pic of Cleo"
