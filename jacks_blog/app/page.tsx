47eb05fd35d714cfd02b1800349de2231c709718
mahj79 <mahonej79@gmail.com>
2024-06-14 15:57:29 -0500

    wrapped p text on home page and added a sentence

--- jacks_blog/app/page.tsx
+++ jacks_blog/app/page.tsx
@@ -3,11 +3,13 @@ import Posts from "./components/Posts";
 export default function Home() {
   return (
     <main className="px-12 mx-auto">
-      <p className="mt-12 mb-12 text-lg text-center dark:text-white">
-        Hello and Welcome!&nbsp;
+      <p className="mt-12 mb-12 text-lg text-center text-wrap dark:text-white">
+        Hello and Welcome!&nbsp; 
         <span className="whitespace-nowrap">
-          I&apos;m <span className="font-bold">Jack!</span>
+          I&apos;m <span className="font-bold">Jack!&nbsp;</span>
         </span>
+        <br></br>
+        Thank you for taking the time to visit my website and hopefuly you find something you enjoy in one of my posts.
       </p>
       <Posts />
     </main>
