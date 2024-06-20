e933510eb0279070a84f278c581a79bee4b6bd63
mahj79 <mahonej79@gmail.com>
2024-06-19 20:47:19 -0500

    some more grammar tweaks

--- jacks_blog/app/page.tsx
+++ jacks_blog/app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
       <p className="px-48 mt-6 mb-12 text-lg text-left text-wrap dark:text-white">
         A little about me is I work in tech, with a background in software engineering and electrical engineering. However, up to this point I've spent my career working in the cybersecurity field. 
 
-        While my focus has been in the cybersecurity space, my real passion is solving complex problems with tech, hence why I spend free time programming! When I'm not working, you can usually find me trying to pick up new skills, spending time with my friends, fiancee, or Cleo my french bulldog. Thank you for taking the time to visit my website and hopefuly you find something you enjoy in one of my posts.
+        While my focus has been in the cybersecurity space, my real passion is solving complex problems with tech, hence why I spend free time programming! When I'm not working, you can usually find me trying to pick up new skills, spending time with my friends, fiancee, or Cleo my french bulldog. Thank you for taking the time to visit my website and hopefully you find something you enjoy in one of my posts.
       </p>
       <Posts />
     </main>
