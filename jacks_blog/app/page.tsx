d142352af122c2f5fc5969588a3b8fa9e95e6874
mahj79 <mahonej79@gmail.com>
2024-06-26 13:57:09 -0500

    updated .next directory

--- jacks_blog/app/page.tsx
+++ jacks_blog/app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
       <p className="px-48 mt-6 mb-12 text-lg text-left text-wrap dark:text-white">
         A little about me is I work in tech&sbquo; with a background in software engineering and electrical engineering. However&sbquo; up to this point I&apos;ve spent my career working in the cybersecurity field. 
 
-        While my focus has been in the cybersecurity space&sbquo; my real passion is solving complex problems with tech&sbquo; hence why I spend free time programming! When I&apos;m not working&sbquo; you can usually find me trying to pick up new skills&sbquo; spending time with my friends&sbquo; fiancee&sbquo; or Cleo my french bulldog. Thank you for taking the time to visit my website and hopefully you find something you enjoy in one of my posts. 
+        While my focus has been in the cybersecurity space&sbquo; my real passion is solving complex problems with tech&sbquo; hence why I spend free time programming! When I&apos;m not working&sbquo; you can usually find me trying to pick up new skills&sbquo; spending time with my friends&sbquo; fiancee&sbquo; or Cleo my french bulldog. Thank you for taking the time to visit my website and hopefully you find something you enjoy in one of my posts.
       </p>
       <Posts />
     </main>
