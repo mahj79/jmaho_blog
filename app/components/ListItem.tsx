4709dabf5a72fd182ef92a5f7db58be2795e56dd
mahj79 <mahonej79@gmail.com>
2024-12-17 20:08:32 -0600

    updated each blog posts section to have image added to each post

--- app/components/ListItem.tsx
+++ app/components/ListItem.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link"
+import Image from "next/image"
 import getFormattedDate from "@/lib/getFormattedDate"
 
 type Props = {
@@ -9,12 +10,25 @@ export default function ListItem({post}: Props) {
     const {id, title, date, description, image} = post;
     const formattedDate = getFormattedDate(date);
     return(
-        <li className="text-slate-100 mt-4 text-2xl dark:text-white/90">
-            <Link className="underline hover:text-black/70 dark:hover:text-white"
-            href={`/posts/${id}`}>{title}</Link>
-            <br/>
-            <p className="text-slate-100 text-sm mt-1">{description}</p>
-            <p className="text-slate-100 text-sm mt-1">{formattedDate}</p>
+        <li className="flex items-start space-x-4 text-slate-100 mt-4 text-2xl dark:text-white/90">
+            {/* Image Container */}
+            <div className="relative w-24 h-24 flex-shrink-0">
+                <Image 
+                    src={`/public/images/${image}`} 
+                    alt={title}
+                    fill
+                    className="object-cover rounded-lg"
+                    sizes="(max-width: 768px) 100px, 100px"
+                />
+            </div>
+            
+            <div>
+                <Link className="underline hover:text-black/70 dark:hover:text-white"
+                href={`/posts/${id}`}>{title}</Link>
+                <br/>
+                <p className="text-slate-100 text-sm mt-1">{description}</p>
+                <p className="text-slate-100 text-sm mt-1">{formattedDate}</p>
+            </div>
         </li>
     )
 }
