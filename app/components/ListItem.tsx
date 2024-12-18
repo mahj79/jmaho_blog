ccf6759e55c51dc5785fbe1d2045e07d56599b50
mahj79 <mahonej79@gmail.com>
2024-12-17 20:57:59 -0600

    added some more updates to have the image for each post show up. Need to finish the description and title

--- app/components/ListItem.tsx
+++ app/components/ListItem.tsx
@@ -11,24 +11,47 @@ export default function ListItem({post}: Props) {
     const formattedDate = getFormattedDate(date);
     return(
         <li className="flex items-start space-x-4 text-slate-100 mt-4 text-2xl dark:text-white/90">
-            {/* Image Container */}
-            <div className="relative w-24 h-24 flex-shrink-0">
-                <Image 
-                    src={image} 
-                    alt={title}
-                    fill
-                    className="object-cover rounded-lg"
-                    sizes="(max-width: 768px) 100px, 100px"
-                />
+            <div 
+                key = {post.id}
+                className="bg-white border rounded-lg overflow-hidden shadow-lg hover:shadow-xl transition-all duration-300 transform hover:-translate-y-2"
+            >
+                {/* Image Container */}
+                <div className="relative w-full h-48">
+                    <Image 
+                        src={image} 
+                        alt={title}
+                        fill
+                        className="object-cover rounded-lg"
+                        sizes="(max-width: 768px) 700px, 700px"
+                    />
+                </div>
+                
+                <div className="p-6">
+                    <div className="flex items-center mb-3">
+                        <span className="text-sm font-medium text-blue-600">
+                            <Link className="underline hover:text-black/70 dark:hover:text-white"
+                                href={`/posts/${id}`}>{title}
+                            </Link>
+                        </span>
+                </div>
+                
+                <div className="flex justify-between items-center text-gray-500 text-sm">
+                    <p className="text-slate-100 text-sm mt-1">
+                        {description}
+                    </p>
+                </div>
+
             </div>
-            
-            <div>
+
+            {/* <div>
                 <Link className="underline hover:text-black/70 dark:hover:text-white"
                 href={`/posts/${id}`}>{title}</Link>
                 <br/>
                 <p className="text-slate-100 text-sm mt-1">{description}</p>
                 <p className="text-slate-100 text-sm mt-1">{formattedDate}</p>
+            </div> */}
             </div>
         </li>
     )
 }
+
