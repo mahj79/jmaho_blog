909c6be450b4008e2e8231e8a4e74985e06f144a
mahj79 <mahonej79@gmail.com>
2025-01-07 10:06:23 -0600

    updated intro bio part

--- app/page.tsx
+++ app/page.tsx
@@ -10,9 +10,9 @@ export default function Home() {
         </span>
         </p>
       <p className="px-8 mt-12 mb-12 text-lg text-left text-wrap dark:text-white">
-        A little about me is I work in tech&sbquo; with a background in software engineering and electrical engineering. However&sbquo; up to this point I&apos;ve spent my career working in the cybersecurity field. 
+        A little about me is I work in tech&sbquo; with a background in software engineering and electrical engineering. I currently work as an Enterprise Architect, however&sbquo; up to this point I&apos;ve spent my career working in the cybersecurity field. 
 
-        While my focus has been in the cybersecurity space&sbquo; my real passion is solving complex problems with tech&sbquo; hence why I spend free time programming! When I&apos;m not working&sbquo; you can usually find me trying to pick up new skills&sbquo; spending time with my friends&sbquo; fiancee&sbquo; or Cleo my french bulldog. Thank you for taking the time to visit my website and hopefully you find something you enjoy in one of my posts.
+        My real passion is solving complex problems with tech&sbquo; hence why I spend free time programming! When I&apos;m not working&sbquo; you can usually find me trying to pick up new skills&sbquo; spending time with my friends&sbquo; fiancee&sbquo; or Cleo my french bulldog. Thank you for taking the time to visit my website and hopefully you find something you enjoy in one of my posts.
       </p>
       <Posts />
     </main>
