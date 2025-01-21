2dd77a3936852ceb06dd22179fe11be98d19130e
Jack <101360909+mahj79@users.noreply.github.com>
2025-01-21 16:05:25 -0600

    updated image size

--- app/components/MyProfilePic.tsx
+++ app/components/MyProfilePic.tsx
@@ -7,8 +7,8 @@ export default function MyProfilePic() {
                 className="border-4 border-black dark:border-slate-500 drop-shadow-xl 
                 shadow-black rounded-3xl mx-auto mt-8"
                 src={"/images/Mahos_Corner_Final.jpg"}
-                width={400}
-                height={200}
+                width={800}
+                height={400}
                 alt="Welcome image of Maho's Corner. Features a desk and various things he associates with such as his french bulldog Cleo,
                 an MSU and Lions helmet, a Tigers baseball bat, the Chicago Skyline, and golf clubs."
                 priority={true}
