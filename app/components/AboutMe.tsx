88cfa60540047425c688cf83b2bb35ececab223b
mahj79 <mahonej79@gmail.com>
2025-01-31 18:30:31 -0600

    we've added an aboutMe file to components. Removed the welcome text.

---
+++ app/components/AboutMe.tsx
@@ -0,0 +1,11 @@
+//About me section
+
+export default function Posts() {
+    
+    return (
+        <section className="text-slate-100 mt-6 mx-auto max-w-2xl">
+            <h2 className="text-4xl font-bold dark:text-white/90">About Me</h2>
+    
+        </section>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,12 @@
 import Posts from "./components/Posts";
+import AboutMe from "./components/AboutMe";
 
 export default function Home() {
   return (
     <main className="px-12 mx-auto">
-      <p className="mt-12 mb-6 text-lg text-center text-wrap dark:text-white">
-        Hello and Welcome!&nbsp; 
-        <span className="text-wrap">
-          I&apos;m <span className="font-bold">Jack</span> or more commonly referred to by what my friends call me&sbquo; <span className="font-bold">Maho!</span>
-        </span>
-        </p>
-      {/* <p className="px-8 mt-12 mb-12 text-lg text-left text-wrap dark:text-white">
-        A little about me is I work in tech&sbquo; with a background in software engineering and electrical engineering. I currently work as an Enterprise Architect, however&sbquo; up to this point I&apos;ve spent my career working in the cybersecurity field. 
-
-        My real passion is solving complex problems with tech&sbquo; hence why I spend free time programming! When I&apos;m not working&sbquo; you can usually find me trying to pick up new skills&sbquo; spending time with my friends&sbquo; fiancee&sbquo; or Cleo my french bulldog. Thank you for taking the time to visit my website and hopefully you find something you enjoy in one of my posts.
-      </p> */}
+        <button className="bg-gray-500 hover:bg-green-400 text-white font-bold py-2 px-4 border-b-4 border-gray-700 hover:border-green-500 rounded">
+          <AboutMe />
+        </button>
       <Posts />
     </main>
   );
