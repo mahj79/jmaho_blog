6cb1fec52687221b70a8b2e01e598976064a58ee
mahj79 <mahonej79@gmail.com>
2025-01-31 19:02:14 -0600

    updated the about me section

--- app/components/AboutMe.tsx
+++ app/components/AboutMe.tsx
@@ -5,13 +5,11 @@ export default function AboutMe() {
     
     return (
         <section className="text-slate-100 mt-6 mx-auto max-w-2xl">
-            <button className="bg-green-900 hover:bg-gray-700 text-white font-bold py-2 px-4 border border-green-300 rounded">
-                {/* <h1 className="text-2xl sm:text-3xl font-bold text-white mb-4 sm:mb-0"> */}
-                    <Link href="/AboutMe" className="text-white/90 no-underline hover:text-white transition-colors duration-200">
-                        About Me
-                    </Link> 
- {/* /               </h1> */}
-            </button>                
+            <h2 className="text-4xl font-bold dark:text-white/90">Jack Mahoney aka Maho</h2>
+            <p className="text-lg text-slate-200 mt-2">
+                Hello and welcome to my blog! Thank you for taking the time
+                to visit my blog and particularly my About Me section.
+            </p>
         </section>
     )
 }
