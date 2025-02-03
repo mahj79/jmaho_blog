71b6f41903fc477ea15fbcf6d654b36cf55c0b71
mahj79 <mahonej79@gmail.com>
2025-02-02 19:20:43 -0600

    updated about me for grammar

--- app/about/AboutMe.tsx
+++ app/about/AboutMe.tsx
@@ -7,7 +7,7 @@ export default function AboutMe() {
         <section className="bg-[#e1dab7] bg-gradient-to-br from-[#e6e6c5] to-[#d8d1a7] text-gray-900 p-4 sm:p-6 rounded-lg border-4 border-black shadow-md my-4 prose-sm sm:prose-xl mx-auto max-w-2xl w-full">
             <h2 className="text-xl sm:text-2xl font-bold underline">Introduction</h2>
             <p className="text-sm sm:text-lg mt-2">
-                Thanks for taking the time to learn more about me, the creator behind Maho&apos;s Corner! My name is Jack Mahoney or more commonly known as what my friends call me, Maho. I am a software engineer / enterprise architect based in Chicago. 
+                Thanks for taking the time to learn more about me, the creator behind Maho&apos;s Corner! My name is Jack Mahoney or what my friends call me, Maho. I am a software engineer / enterprise architect based in Chicago. 
                 <Image
                 className="border-4 border-black dark:border-slate-500 drop-shadow-xl 
                 shadow-black rounded-3xl mx-auto mt-4 mb-4"
@@ -17,10 +17,10 @@ export default function AboutMe() {
                 alt="Profile picture of Jack Mahoney"
                 priority={true}
             />
-                In my continuous pursuit to be a lifelong learner, I decided to create this blog &quot;Maho&apos;s Corner&quot; in early 2024 to tone up some of my front end programming skills. But also to write about topics I found interesting, as I find writing a bit therapeutic. The inspiration behind Maho&apos;s Corner is to approach traditionally complex technical subjects like Machine Learning and break them down to be simple enough for anyone to walk away from an article and feel like they&apos;ve learned something. The other part is to write about things I enjoy, such as cooking, sports, and being there for the people I care about. 
+                In my continuous pursuit to be a lifelong learner, I decided to create this blog &quot;Maho&apos;s Corner&quot; in early 2024 to tone up some of my front end programming skills, but also to write about topics I find interesting, as writing is a bit therapeutic for me. The inspiration behind Maho&apos;s Corner is to approach traditionally complex technical subjects like Machine Learning and break them down to be simple enough for anyone to walk away from an article and feel like they&apos;ve learned something. The other part is to write about things I enjoy, such as cooking, sports, and being there for the people I care about. 
                 <br />
                 <br />
-                Maho&apos;s Corner is meant to be a place where all are welcome. It is a place for learners of all ages and any background. You are welcome to reach out to me on any of my socials linked in the navbar. If you are particularly interested in a subject and want me to write about it, then by all means, contact me via one of my socials, or my email: mahonej79@gmail.com and tell me about what you&apos;re interested in!
+                Maho&apos;s Corner is meant to be a place where all are welcome. It is a place for learners of all ages and any background. You are welcome to reach out to me on any of my socials which are linked in the navbar. If you are particularly interested in a subject and want me to write about it, then by all means, contact me via one of my socials, or my email: mahonej79@gmail.com and tell me about what you&apos;re interested in!
                 <br />
                 <br />
                 <br />
@@ -28,13 +28,13 @@ export default function AboutMe() {
 
             <h2 className="text-xl sm:text-2xl font-bold underline mt-4">Background</h2>
             <p className="text-sm sm:text-lg mt-2 mb-4">
-                While my job titles may imply that all I do is code, I actually like to think of myself simply as a problem solver. Throughout my whole life and especially since I started my career, I became known as the guy who would find an answer to a complicated problem. I think this is mostly in part for a few reasons. 
+                While my job titles may imply that all I do is code, I actually like to think of myself as simply a problem solver. Throughout my life and especially since I started my career, I became known as the guy who would find an answer to a complicated problem. I think this is mostly in part for a few reasons. 
                 <br />
                 <br />
-                With the main reason being I simply love solving problems. I think ever since I was young, I would spend hours solving puzzles growing up. Then as I got older I gravitated towards engineering because that&apos;s where the challenging problems were. I would get a sense of satisfaction for completing something complex, that you just don&apos;t get from figuring out simple or menial tasks. There&apos;s joy derived from putting in the effort and recognizing the effort it takes to achieve the outcome you want. 
+                I&apos;ll start with the main reason being I simply love solving problems. I think ever since I was young I would spend hours solving puzzles. Then as I got older I gravitated towards engineering because that&apos;s where the challenging problems were. I would get a sense of satisfaction from completing something complex, and you just don&apos;t get that from figuring out simple or menial tasks. There&apos;s joy derived from putting in the effort and recognizing the effort it takes to achieve the outcome you want. 
                 <br />
                 <br />
-                Speaking of growing up, I grew up in the middle of Michigan, near the state&apos;s capitol; Lansing. After high school, I attended Michigan State University and studied Electrical and Computer Engineering. This is where I first learned to code and understand hardware at the most granular of levels. The first two programming languages I learned were C and Assembly, because for whatever reason electrical engineers have to turn the clock back to the stone ages to truly understand the inner workings of a computer. After 4 years I graduated and received a degree in Electrical and Computer Engineering. I am particularly proud of this, as I am a first generation college graduate. At Michigan State is where I first learned to code. 
+                Speaking of growing up, I grew up in the middle of Michigan, near the state&apos;s capitol; Lansing. After high school, I attended Michigan State University and studied Electrical and Computer Engineering. This is where I first learned to code and understand hardware at the most granular of levels. The first two programming languages I learned were C and Assembly, because for whatever reason electrical engineers have to turn the clock back to the stone ages to truly understand the inner workings of a computer. After 4 years I graduated and received a degree in Electrical and Computer Engineering. I am particularly proud of this, as I am a first generation college graduate.
                 <br />
                 <br />
                 Following graduation, I moved to Chicago to live with my best friends. I always wanted to live in a big city and in my completely biased opinion, Chicago is one of the best cities in the world so naturally it made sense. Not really knowing what I wanted to do, I spent a few years in cybersecurity consulting. I learned a lot, but after getting burned out, I realized I wanted to do a small career pivot. I wanted to get further back into software engineering and also travel less, so that&apos;s how I ended up at my current job as an enterprise architect for a big bank. 
@@ -48,7 +48,7 @@ export default function AboutMe() {
 
             <h2 className="text-xl sm:text-2xl font-bold underline mt-4">Resume</h2>
             <div className="text-sm sm:text-lg mt-2 mb-4">
-                Jack is a skilled engineer who has an amazing track record of solving complex problems at various companies, in various industries. He is typically known as someone who isn&apos;t easily phased by pressure or stress. He&apos;s even been described as &quot;a serial smiler&quot;, due to his tendencies of taking a glass half full approach to situations and helping diffuse tension across teams. Many view him as a mixture of a leader and individual contributor, thanks to his flexibility to adapt to whatever the situation calls for. 
+                Jack is a skilled engineer who has an impressive track record of solving complex problems at various companies, in various industries. He is typically known as someone who isn&apos;t easily phased by pressure or stress. He&apos;s even been described as &quot;a serial smiler&quot;, due to his tendencies of taking a glass half full approach to situations and helping diffuse tension across teams. Many view him as a mixture of a leader and individual contributor thanks to his ability to adapt to whatever the situation calls for. 
                 <br />
                 <br />
                 <h4 className="font-bold underline">Lead Enterprise Architect (2022 - Present)</h4>
