49f94815950805f399a58db09a9c78f31bfd44b6
mahj79 <mahonej79@gmail.com>
2025-02-04 20:13:48 -0600

    updated it so posts are displayed in grid style

--- app/components/Posts.tsx
+++ app/components/Posts.tsx
@@ -5,13 +5,15 @@ export default function Posts() {
     const posts = getSortedPostsData()
     
     return (
-        <section className="text-slate-100 mt-6 mx-auto max-w-2xl">
-            <h2 className="text-4xl font-bold dark:text-white/90">Blog Posts</h2>
-            <ul className="w-full">
+        <>
+            <h2 className="text-4xl font-bold dark:text-white/90 text-center mb-6">
+                Blog Posts
+            </h2>
+            <section className="mt-6 mx-auto max-w-6xl grid grid-cols-1 sm:grid-cols-2 gap-6 px-6 list-none">
                 {posts.map(post => (
                     <ListItem key={post.id} post={post} />
                 ))}
-            </ul>
-        </section>
+            </section>
+        </>
     )
 }
